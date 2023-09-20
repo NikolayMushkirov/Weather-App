85d99c4c1542ec6b5d322cf6fab2e26af00e30f8
Nikolay <mushk111@yandex.ru>
2023-09-20 10:03:17 +0800

    Fixed: App.tsx file

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,19 @@
 import { Routes, Route } from "react-router-dom";
 
-import useLocalStorage from "use-local-storage";
-
 import MainPage from "pages/MainPage/MainPage";
 import DetailedForecastPage from "pages/DetailedForecastPage/DetailedForecastPage";
 
-import "./App.scss";
 import useTheme from "hooks/useTheme";
 
-function App() {
+import "./App.scss";
 
-  const {theme} = useTheme()
+function App() {
+  const { theme } = useTheme();
 
   return (
     <div className="App" data-theme={theme}>
       <Routes>
-        <Route
-          path="/"
-          element={<MainPage  />}
-        />
+        <Route path="/" element={<MainPage />} />
         <Route path="forecast" element={<DetailedForecastPage />} />
       </Routes>
     </div>
