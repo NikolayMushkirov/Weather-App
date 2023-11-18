83c0b2e60ca0b0d6de76bc1eb863302a9ed72ebb
Nikolay <mushk111@yandex.ru>
2023-11-18 18:49:36 +0800

    Improved: code in useWeatherData hook

--- src/hooks/useWeatherData.tsx
+++ src/hooks/useWeatherData.tsx
@@ -9,23 +9,28 @@ const useWeatherData = () => {
   const fetchWeatherData = async () => {
     const API_KEY = import.meta.env.VITE_WEATHER_APP_API_KEY;
     const weatherDataUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${geolocation?.lat}&lon=${geolocation?.lon}&q=${searchValue}&appid=${API_KEY}&units=metric`;
+    const airQualUrl = `https://api.openweathermap.org/data/2.5/air_pollution/forecast?lat=${geolocation?.lat}&lon=${geolocation?.lon}&appid=${API_KEY}&units=metric`;
 
-    const weatherDataResponse = await fetch(weatherDataUrl);
+    const [weatherDataResponse, airQualResponse] = await Promise.all([
+      fetch(weatherDataUrl),
+      fetch(airQualUrl),
+    ]);
+
     if (!weatherDataResponse.ok) {
-      throw new Error("Network response was not ok");
+      throw new Error(
+        `Network response for weather data was not ok: ${weatherDataResponse.status} - ${weatherDataResponse.statusText}`
+      );
     }
-    const forecastData = await weatherDataResponse.json();
-
-    const airQualLat = await forecastData.city.coord.lat;
-    const airQualLon = await forecastData.city.coord.lon;
-
-    const airQualUrl = `https://api.openweathermap.org/data/2.5/air_pollution/forecast?lat=${airQualLat}&lon=${airQualLon}&appid=${API_KEY}&units=metric`;
-
-    const airQualResponse = await fetch(airQualUrl);
     if (!airQualResponse.ok) {
-      throw new Error("Network response was not ok");
+      throw new Error(
+        `Network response for air quality data was not ok: ${airQualResponse.status} - ${airQualResponse.statusText}`
+      );
     }
-    const airData = await airQualResponse.json();
+
+    const [forecastData, airData] = await Promise.all([
+      weatherDataResponse.json(),
+      airQualResponse.json(),
+    ]);
 
     return { forecastData, airData };
   };
@@ -38,9 +43,10 @@ const useWeatherData = () => {
     }
   );
 
-  const sortedWeatherData = data?.forecastData.list.filter(
+  const filteredWeatherData = data?.forecastData.list.filter(
     (item: { dt_txt: string }) => item.dt_txt.endsWith("15:00:00")
   );
+  const sortedWeatherData = filteredWeatherData;
   return { data, sortedWeatherData, isLoading };
 };
 
